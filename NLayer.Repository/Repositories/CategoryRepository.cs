6f4799c7f1bc8113f60e726def4df5ecb4edf1f1
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-07-24 21:05:31 +0300

    [DEV-UP]: Categories Controller , ICategoryRepository , CategoryRepository added.

---
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync(); // FirstOrDefault ile farkı ; Birden fazla Id'den ilkini alır.
+                                                                                                                             // SingleOrDefault ise ,  Tek bir tane Id olması gerektiği için , hata döner.  
+        }
+    }
+}
