691bdc557284e949cecdfa091dc357d1aa1dc966
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-07-24 03:04:18 +0300

    [DEV-UP] : CustomBaseController and ProductsController added.

---
+++ NLayer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction] // Endpoint olmadığı için Swagger'ın Endpoint olarak algılamaması için NoAction ekledik.
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204) // 204 : NoContent HTTP durum kodu. 
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode// Status 200 ise 200 , 404 dönüyorsa geriye 404 döneceğiz.
+            };
+        }
+    }
+}
---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+
+    public class ProductsController : CustomBaseController // Business kod yani Service kodu buraya Best Practice için yazılmayacak. Olabildiğince temiz olmalı.
+    {
+        // Controller sadece servisleri bilir. Kesinlikle repository miras almayacak.
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        // GET api/products
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList()); // IEnumerable yani bir Entity döneceği için mapper'ı da ekledik.
+
+            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+        // Örnek : api/products/5 olan data gelir.
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+
+            var productsDtos = _mapper.Map<ProductDto>(product);
+
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDtos));
+
+
+        }
+
+    }
+}
