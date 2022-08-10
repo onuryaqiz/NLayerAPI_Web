9420212794474355a197a4f78395dbee805feab1
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-08-10 11:06:35 +0300

    ProductsController modified.

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -6,10 +10,13 @@ namespace NLayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _services;
-
-        public ProductsController(IProductService services)
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public ProductsController(IProductService services, ICategoryService categoryService, IMapper mapper)
         {
             _services = services;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
@@ -17,5 +24,30 @@ namespace NLayer.Web.Controllers
 
             return View(await _services.GetProductWithCategory());
         }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+
+            var categoriesDTO = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.cagetories = new SelectList(categoriesDTO, "Id", "Name"); // DropdownList olarak SelectList alacak . categoriesDTO liste olarak verdik. Bu categoryDTO'dan ID'yi göstereceğiz . Kullanıcılar da Name'ini görecek.
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+
+            {
+                await _services.AddAsync(_mapper.Map<Product>(productDto)); // ProductDto'yu Product'a dönüştür. 
+
+                return RedirectToAction(nameof(Index)); // "Index" olarak da yazılabilir fakat tip güvenli şekilde yazdık.
+
+                return View(); // Başarısız ise View'e geri dönecek . 
+            }
+        }
+
+
     }
 }
