76971ebafcf9a22b061d52e54dd9f8298a4938cb
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-08-10 10:21:54 +0300

    RepoServiceModule added , Programs.cs modified

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -1,7 +1,34 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using NLayer.Web.Modules;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(MapProfile));
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        //                            ("NLayer.Repository")
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); //NLayer.Repository yazmak yerine isim değişikliği yerine 
+                                                                                              //dinamik hale getirerek Assembly'nin(NLAyer.Repository) ismini aldık. 
+
+
+    });
+
+
+
+});
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule())); // Yazdığımız module burada eklendi. 
 
 var app = builder.Build();
 
